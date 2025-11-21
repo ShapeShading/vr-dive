1ce0a68d68c72e39027b24ada6a9e956c04c3429
tiye <jiyinyiyong@gmail.com>
2025-11-22 01:56:47 +0800

    at least cudes are drawn

--- vr-dive/Renderer/Renderer.swift
+++ vr-dive/Renderer/Renderer.swift
@@ -5,10 +5,35 @@ import MetalKit
 import Spatial
 import SwiftUI
 
-// Simple uniforms struct matching the Metal shader
-struct Uniforms {
+struct BackgroundUniforms {
   var time: Float
-  var padding: SIMD3<Float>
+  var intensity: Float
+  var padding: SIMD2<Float> = .zero
+}
+
+struct SceneUniforms {
+  var viewProjectionMatrix: simd_float4x4
+  var time: Float
+  var layerCount: UInt32
+  var padding: SIMD2<Float> = .zero
+}
+
+struct SimulationUniforms {
+  var deltaTime: Float
+  var globalTime: Float
+  var objectCount: UInt32
+  var padding: UInt32 = 0
+}
+
+struct MeshVertex {
+  var position: SIMD3<Float>
+  var normal: SIMD3<Float>
+}
+
+struct ObjectState {
+  var positionAndType: SIMD4<Float>
+  var velocityAndPhase: SIMD4<Float>
+  var scaleAndPadding: SIMD4<Float>
 }
 
 struct VRConfiguration: CompositorLayerConfiguration {
@@ -31,10 +56,19 @@ class Renderer {
   let layerRenderer: LayerRenderer
   let device: MTLDevice
   let commandQueue: MTLCommandQueue
-  let pipelineState: MTLRenderPipelineState
+  let backgroundPipelineState: MTLRenderPipelineState
+  let objectPipelineState: MTLRenderPipelineState
+  let computePipelineState: MTLComputePipelineState
+  let depthStencilState: MTLDepthStencilState
+  let meshVertexBuffer: MTLBuffer
+  let meshIndexBuffer: MTLBuffer
+  let meshIndexCount: Int
+  let objectStateBuffer: MTLBuffer
   let arSession: ARKitSession
   let worldTracking: WorldTrackingProvider
   private var lastKnownDeviceAnchor: ARKit.DeviceAnchor?
+  private var lastSimulationTimestamp: Float = 0
+  private let objectCount = 48
 
   var startTime: Date = Date()
   private static let attosecondsPerSecond = 1_000_000_000_000_000_000.0
@@ -45,22 +79,17 @@ class Renderer {
     self.commandQueue = self.device.makeCommandQueue()!
 
     let library = device.makeDefaultLibrary()!
-    let vertexFunction = library.makeFunction(name: "vertexShader")
-    let fragmentFunction = library.makeFunction(name: "fragmentShader")
+    self.backgroundPipelineState = try! Renderer.makeBackgroundPipelineState(device: device, library: library)
+    self.objectPipelineState = try! Renderer.makeObjectPipelineState(device: device, library: library)
+    self.computePipelineState = try! Renderer.makeComputePipelineState(device: device, library: library)
+    self.depthStencilState = Renderer.makeDepthStencilState(device: device)
 
-    let pipelineDescriptor = MTLRenderPipelineDescriptor()
-    pipelineDescriptor.vertexFunction = vertexFunction
-    pipelineDescriptor.fragmentFunction = fragmentFunction
-    pipelineDescriptor.colorAttachments[0].pixelFormat = .rgba16Float
-    pipelineDescriptor.colorAttachments[0].isBlendingEnabled = true
-    pipelineDescriptor.colorAttachments[0].sourceRGBBlendFactor = .sourceAlpha
-    pipelineDescriptor.colorAttachments[0].destinationRGBBlendFactor = .oneMinusSourceAlpha
-    pipelineDescriptor.colorAttachments[0].sourceAlphaBlendFactor = .one
-    pipelineDescriptor.colorAttachments[0].destinationAlphaBlendFactor = .zero
-    pipelineDescriptor.depthAttachmentPixelFormat = .depth32Float
-    pipelineDescriptor.inputPrimitiveTopology = .triangle
+    let geometry = Renderer.makeCubeGeometry(device: device)
+    self.meshVertexBuffer = geometry.vertexBuffer
+    self.meshIndexBuffer = geometry.indexBuffer
+    self.meshIndexCount = geometry.indexCount
 
-    self.pipelineState = try! device.makeRenderPipelineState(descriptor: pipelineDescriptor)
+    self.objectStateBuffer = Renderer.makeInitialObjectStates(device: device, count: objectCount)
 
     self.arSession = ARKitSession()
     self.worldTracking = WorldTrackingProvider()
@@ -139,6 +168,8 @@ class Renderer {
         print("[Renderer] Waiting for reliable world tracking data...")
       }
 
+      simulateObjectsIfNeeded(currentTime: animationTime)
+
       autoreleasepool {
         frame.startSubmission()
         defer { frame.endSubmission() }
@@ -175,12 +206,8 @@ class Renderer {
     if let renderPassDescriptor = makeRenderPassDescriptor(for: drawable),
       let encoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
     {
-      encoder.setRenderPipelineState(pipelineState)
-
-      var uniforms = Uniforms(time: time, padding: SIMD3<Float>(repeating: 0))
-      encoder.setVertexBytes(&uniforms, length: MemoryLayout<Uniforms>.size, index: 0)
-      encoder.setFragmentBytes(&uniforms, length: MemoryLayout<Uniforms>.size, index: 0)
-      encoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: 3, instanceCount: 2)
+      encodeBackgroundPass(with: encoder, time: time)
+      encodeObjectPass(with: encoder, drawable: drawable, time: time)
       encoder.endEncoding()
     }
 
@@ -217,5 +244,263 @@ class Renderer {
     }
 
     return descriptor
+  }
+
+  private func encodeBackgroundPass(with encoder: MTLRenderCommandEncoder, time: Float) {
+    var uniforms = BackgroundUniforms(time: time, intensity: 0.85)
+    encoder.setRenderPipelineState(backgroundPipelineState)
+    encoder.setVertexBytes(&uniforms, length: MemoryLayout<BackgroundUniforms>.stride, index: 0)
+    encoder.setFragmentBytes(&uniforms, length: MemoryLayout<BackgroundUniforms>.stride, index: 0)
+    encoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: 3, instanceCount: 2)
+  }
+
+  private func encodeObjectPass(
+    with encoder: MTLRenderCommandEncoder,
+    drawable: LayerRenderer.Drawable,
+    time: Float
+  ) {
+    guard let colorTexture = drawable.colorTextures.first else { return }
+    encoder.setRenderPipelineState(objectPipelineState)
+    encoder.setDepthStencilState(depthStencilState)
+    encoder.setCullMode(.back)
+    encoder.setFrontFacing(.counterClockwise)
+
+    encoder.setVertexBuffer(meshVertexBuffer, offset: 0, index: 0)
+    encoder.setVertexBuffer(objectStateBuffer, offset: 0, index: 1)
+
+    let aspect = Float(colorTexture.width) / Float(max(colorTexture.height, 1))
+    let projection = simd_float4x4.perspective(fovY: 60 * (.pi / 180), aspect: aspect, nearZ: 0.05, farZ: 20)
+    let view = simd_float4x4.lookAt(
+      eye: SIMD3<Float>(0, 0, 0.4),
+      center: SIMD3<Float>(0, 0, -1),
+      up: SIMD3<Float>(0, 1, 0)
+    )
+    let layerCount = UInt32(max(colorTexture.arrayLength, 1))
+    var sceneUniforms = SceneUniforms(viewProjectionMatrix: projection * view, time: time, layerCount: layerCount)
+
+    encoder.setVertexBytes(&sceneUniforms, length: MemoryLayout<SceneUniforms>.stride, index: 2)
+    encoder.setFragmentBytes(&sceneUniforms, length: MemoryLayout<SceneUniforms>.stride, index: 0)
+
+    encoder.drawIndexedPrimitives(
+      type: .triangle,
+      indexCount: meshIndexCount,
+      indexType: .uint16,
+      indexBuffer: meshIndexBuffer,
+      indexBufferOffset: 0,
+      instanceCount: objectCount * Int(layerCount)
+    )
+  }
+
+  private func simulateObjectsIfNeeded(currentTime: Float) {
+    let deltaTime = max(0, currentTime - lastSimulationTimestamp)
+    guard deltaTime > 0 else { return }
+
+    var uniforms = SimulationUniforms(
+      deltaTime: min(deltaTime, 1.0 / 30.0),
+      globalTime: currentTime,
+      objectCount: UInt32(objectCount)
+    )
+
+    guard let commandBuffer = commandQueue.makeCommandBuffer(),
+      let encoder = commandBuffer.makeComputeCommandEncoder()
+    else { return }
+
+    encoder.setComputePipelineState(computePipelineState)
+    encoder.setBuffer(objectStateBuffer, offset: 0, index: 0)
+    encoder.setBytes(&uniforms, length: MemoryLayout<SimulationUniforms>.stride, index: 1)
+
+    let threadWidth = min(computePipelineState.maxTotalThreadsPerThreadgroup, 32)
+    let threadsPerThreadgroup = MTLSize(width: threadWidth, height: 1, depth: 1)
+    let threadgroups = MTLSize(
+      width: (objectCount + threadWidth - 1) / threadWidth,
+      height: 1,
+      depth: 1
+    )
+
+    encoder.dispatchThreadgroups(threadgroups, threadsPerThreadgroup: threadsPerThreadgroup)
+    encoder.endEncoding()
+    commandBuffer.commit()
+    commandBuffer.waitUntilCompleted()
+
+    lastSimulationTimestamp = currentTime
+  }
+}
+
+private extension Renderer {
+  static func makeBackgroundPipelineState(device: MTLDevice, library: MTLLibrary) throws -> MTLRenderPipelineState {
+    let descriptor = MTLRenderPipelineDescriptor()
+    descriptor.vertexFunction = library.makeFunction(name: "backgroundVertexShader")
+    descriptor.fragmentFunction = library.makeFunction(name: "backgroundFragmentShader")
+    descriptor.colorAttachments[0].pixelFormat = .rgba16Float
+    descriptor.depthAttachmentPixelFormat = .depth32Float
+    descriptor.inputPrimitiveTopology = .triangle
+    return try device.makeRenderPipelineState(descriptor: descriptor)
+  }
+
+  static func makeObjectPipelineState(device: MTLDevice, library: MTLLibrary) throws -> MTLRenderPipelineState {
+    let descriptor = MTLRenderPipelineDescriptor()
+    descriptor.vertexFunction = library.makeFunction(name: "objectVertexShader")
+    descriptor.fragmentFunction = library.makeFunction(name: "objectFragmentShader")
+    descriptor.colorAttachments[0].pixelFormat = .rgba16Float
+    descriptor.depthAttachmentPixelFormat = .depth32Float
+    descriptor.inputPrimitiveTopology = .triangle
+
+    let vertexDescriptor = MTLVertexDescriptor()
+    vertexDescriptor.attributes[0].format = .float3
+    vertexDescriptor.attributes[0].offset = 0
+    vertexDescriptor.attributes[0].bufferIndex = 0
+    vertexDescriptor.attributes[1].format = .float3
+    vertexDescriptor.attributes[1].offset = MemoryLayout<SIMD3<Float>>.stride
+    vertexDescriptor.attributes[1].bufferIndex = 0
+    vertexDescriptor.layouts[0].stride = MemoryLayout<MeshVertex>.stride
+    descriptor.vertexDescriptor = vertexDescriptor
+
+    return try device.makeRenderPipelineState(descriptor: descriptor)
+  }
+
+  static func makeComputePipelineState(device: MTLDevice, library: MTLLibrary) throws -> MTLComputePipelineState {
+    let function = library.makeFunction(name: "simulateObjects")!
+    return try device.makeComputePipelineState(function: function)
+  }
+
+  static func makeDepthStencilState(device: MTLDevice) -> MTLDepthStencilState {
+    let descriptor = MTLDepthStencilDescriptor()
+    descriptor.depthCompareFunction = .less
+    descriptor.isDepthWriteEnabled = true
+    return device.makeDepthStencilState(descriptor: descriptor)!
+  }
+
+  static func makeCubeGeometry(device: MTLDevice) -> (vertexBuffer: MTLBuffer, indexBuffer: MTLBuffer, indexCount: Int) {
+    let vertices: [MeshVertex] = [
+      // Front
+      MeshVertex(position: [-0.5, -0.5, 0.5], normal: [0, 0, 1]),
+      MeshVertex(position: [0.5, -0.5, 0.5], normal: [0, 0, 1]),
+      MeshVertex(position: [0.5, 0.5, 0.5], normal: [0, 0, 1]),
+      MeshVertex(position: [-0.5, 0.5, 0.5], normal: [0, 0, 1]),
+      // Back
+      MeshVertex(position: [-0.5, -0.5, -0.5], normal: [0, 0, -1]),
+      MeshVertex(position: [0.5, -0.5, -0.5], normal: [0, 0, -1]),
+      MeshVertex(position: [0.5, 0.5, -0.5], normal: [0, 0, -1]),
+      MeshVertex(position: [-0.5, 0.5, -0.5], normal: [0, 0, -1]),
+      // Left
+      MeshVertex(position: [-0.5, -0.5, -0.5], normal: [-1, 0, 0]),
+      MeshVertex(position: [-0.5, -0.5, 0.5], normal: [-1, 0, 0]),
+      MeshVertex(position: [-0.5, 0.5, 0.5], normal: [-1, 0, 0]),
+      MeshVertex(position: [-0.5, 0.5, -0.5], normal: [-1, 0, 0]),
+      // Right
+      MeshVertex(position: [0.5, -0.5, -0.5], normal: [1, 0, 0]),
+      MeshVertex(position: [0.5, -0.5, 0.5], normal: [1, 0, 0]),
+      MeshVertex(position: [0.5, 0.5, 0.5], normal: [1, 0, 0]),
+      MeshVertex(position: [0.5, 0.5, -0.5], normal: [1, 0, 0]),
+      // Top
+      MeshVertex(position: [-0.5, 0.5, 0.5], normal: [0, 1, 0]),
+      MeshVertex(position: [0.5, 0.5, 0.5], normal: [0, 1, 0]),
+      MeshVertex(position: [0.5, 0.5, -0.5], normal: [0, 1, 0]),
+      MeshVertex(position: [-0.5, 0.5, -0.5], normal: [0, 1, 0]),
+      // Bottom
+      MeshVertex(position: [-0.5, -0.5, 0.5], normal: [0, -1, 0]),
+      MeshVertex(position: [0.5, -0.5, 0.5], normal: [0, -1, 0]),
+      MeshVertex(position: [0.5, -0.5, -0.5], normal: [0, -1, 0]),
+      MeshVertex(position: [-0.5, -0.5, -0.5], normal: [0, -1, 0]),
+    ]
+
+    let indices: [UInt16] = [
+      0, 1, 2, 0, 2, 3,
+      4, 6, 5, 4, 7, 6,
+      8, 9, 10, 8, 10, 11,
+      12, 14, 13, 12, 15, 14,
+      16, 17, 18, 16, 18, 19,
+      20, 22, 21, 20, 23, 22,
+    ]
+
+    let vertexBuffer = device.makeBuffer(
+      bytes: vertices,
+      length: MemoryLayout<MeshVertex>.stride * vertices.count,
+      options: [.storageModeShared]
+    )!
+    let indexBuffer = device.makeBuffer(
+      bytes: indices,
+      length: MemoryLayout<UInt16>.stride * indices.count,
+      options: [.storageModeShared]
+    )!
+
+    return (vertexBuffer, indexBuffer, indices.count)
+  }
+
+  static func makeInitialObjectStates(device: MTLDevice, count: Int) -> MTLBuffer {
+    var states: [ObjectState] = []
+    states.reserveCapacity(count)
+
+    for i in 0..<count {
+      let type: Float = (i % 7 == 0) ? 1.0 : 0.0
+      let zRange: ClosedRange<Float> = -3.2...(-0.8)
+      let position = SIMD3<Float>(
+        Float.random(in: -1.5...1.5),
+        Float.random(in: -0.9...1.1),
+        Float.random(in: zRange)
+      )
+      let baseSpeed: Float = type > 0.5 ? 0.15 : 0.1
+      let velocity = SIMD3<Float>(
+        Float.random(in: -baseSpeed...baseSpeed) * 0.1,
+        Float.random(in: -baseSpeed...baseSpeed) * 0.2,
+        Float.random(in: -baseSpeed...baseSpeed)
+      )
+      let scale = SIMD3<Float>(
+        Float.random(in: 0.15...0.35) * (type > 0.5 ? 1.8 : 1.0),
+        Float.random(in: 0.15...0.45),
+        Float.random(in: 0.15...0.35)
+      )
+      let phase = Float.random(in: 0...(.pi * 2))
+
+      states.append(
+        ObjectState(
+          positionAndType: SIMD4<Float>(position, type),
+          velocityAndPhase: SIMD4<Float>(velocity, phase),
+          scaleAndPadding: SIMD4<Float>(scale, 0)
+        )
+      )
+    }
+
+    return device.makeBuffer(
+      bytes: states,
+      length: MemoryLayout<ObjectState>.stride * states.count,
+      options: [.storageModeShared]
+    )!
+  }
+}
+
+private extension simd_float4x4 {
+  static func perspective(fovY: Float, aspect: Float, nearZ: Float, farZ: Float) -> simd_float4x4 {
+    let yScale = 1 / tan(fovY * 0.5)
+    let xScale = yScale / max(aspect, 0.1)
+    let zRange = farZ - nearZ
+    let zScale = -(farZ + nearZ) / zRange
+    let wzScale = -(2 * farZ * nearZ) / zRange
+
+    return simd_float4x4(
+      SIMD4<Float>(xScale, 0, 0, 0),
+      SIMD4<Float>(0, yScale, 0, 0),
+      SIMD4<Float>(0, 0, zScale, -1),
+      SIMD4<Float>(0, 0, wzScale, 0)
+    )
+  }
+
+  static func lookAt(eye: SIMD3<Float>, center: SIMD3<Float>, up: SIMD3<Float>) -> simd_float4x4 {
+    let forward = simd_normalize(center - eye)
+    let right = simd_normalize(simd_cross(forward, up))
+    let correctedUp = simd_cross(right, forward)
+
+    let translation = SIMD3<Float>(
+      -simd_dot(right, eye),
+      -simd_dot(correctedUp, eye),
+      simd_dot(forward, eye)
+    )
+
+    return simd_float4x4(
+      SIMD4<Float>(right.x, correctedUp.x, -forward.x, 0),
+      SIMD4<Float>(right.y, correctedUp.y, -forward.y, 0),
+      SIMD4<Float>(right.z, correctedUp.z, -forward.z, 0),
+      SIMD4<Float>(translation.x, translation.y, translation.z, 1)
+    )
   }
 }
--- vr-dive/Renderer/Shaders.metal
+++ vr-dive/Renderer/Shaders.metal
@@ -1,24 +1,56 @@
 #include <metal_stdlib>
 using namespace metal;
 
-struct Uniforms {
+struct BackgroundUniforms {
   float time;
-  float3 padding;
+  float intensity;
+  float2 padding;
 };
 
-struct VertexOut {
+struct SceneUniforms {
+  float4x4 viewProjectionMatrix;
+  float time;
+  uint layerCount;
+  float2 padding;
+};
+
+struct SimulationUniforms {
+  float deltaTime;
+  float globalTime;
+  uint objectCount;
+  uint padding;
+};
+
+struct MeshVertex {
+  float3 position;
+  float3 normal;
+};
+
+struct ObjectState {
+  float4 positionAndType;
+  float4 velocityAndPhase;
+  float4 scaleAndPadding;
+};
+
+struct BackgroundVertexOut {
   float4 position [[position]];
   float2 uv;
   uint layer [[render_target_array_index]];
 };
 
-// Simple test shaders
+struct ObjectVertexOut {
+  float4 position [[position]];
+  float3 normal;
+  float3 worldPos;
+  uint layer [[render_target_array_index]];
+  float objectType;
+};
 
-vertex VertexOut vertexShader(uint vertexID [[vertex_id]],
-                              uint instanceID [[instance_id]],
-                              constant Uniforms &uniforms [[buffer(0)]]) {
-  VertexOut out;
-  // Full screen triangle
+vertex BackgroundVertexOut backgroundVertexShader(
+  uint vertexID [[vertex_id]],
+  uint instanceID [[instance_id]],
+  constant BackgroundUniforms &uniforms [[buffer(0)]]) {
+  BackgroundVertexOut out;
   float2 positions[3] = {float2(-1, -1), float2(3, -1), float2(-1, 3)};
   out.position = float4(positions[vertexID], 0.0, 1.0);
   out.uv = positions[vertexID] * 0.5 + 0.5;
@@ -26,25 +58,107 @@ vertex VertexOut vertexShader(uint vertexID [[vertex_id]],
   return out;
 }
 
-fragment float4 fragmentShader(VertexOut in [[stage_in]],
-                               constant Uniforms &uniforms [[buffer(0)]]) {
-  // Bright test pattern with animated colors
-  float t = uniforms.time * 0.5;
+float3 shallowSeaGradient(float2 uv, float t, float intensity) {
+  float depthFactor = smoothstep(0.0, 1.0, uv.y);
+  float3 topColor = float3(0.05, 0.19, 0.24);
+  float3 bottomColor = float3(0.01, 0.07, 0.11);
+  float gentlePulse = 0.03 * sin(t * 0.3 + uv.x * 2.0);
+  return mix(bottomColor, topColor, depthFactor) * (intensity + gentlePulse);
+}
 
-  // Create a colorful gradient
-  float3 col =
-      float3(0.5 + 0.5 * sin(t + in.uv.x * 3.14159),
-             0.5 + 0.5 * cos(t + in.uv.y * 3.14159), 0.5 + 0.5 * sin(t * 2.0));
+fragment float4 backgroundFragmentShader(
+  BackgroundVertexOut in [[stage_in]],
+  constant BackgroundUniforms &uniforms [[buffer(0)]]) {
+  float3 color = shallowSeaGradient(in.uv, uniforms.time, uniforms.intensity);
+  float caustic = 0.08 * sin((in.uv.x + uniforms.time * 0.1) * 12.0) *
+                  sin((in.uv.y + uniforms.time * 0.15) * 8.0);
+  color += caustic;
 
-  // Make colors brighter and more saturated
-  col = col * 0.8 + 0.2;
-
-  // Different tint per eye to verify stereo
   if (in.layer == 0) {
-    col.r = mix(col.r, 1.0, 0.3); // Left eye more red
+    color *= float3(1.0, 0.98, 0.95);
   } else {
-    col.b = mix(col.b, 1.0, 0.3); // Right eye more blue
+    color *= float3(0.95, 0.98, 1.0);
   }
 
-  return float4(col, 1.0);
+  return float4(color, 1.0);
+}
+
+vertex ObjectVertexOut objectVertexShader(
+  const device MeshVertex *vertices [[buffer(0)]],
+  const device ObjectState *states [[buffer(1)]],
+  constant SceneUniforms &uniforms [[buffer(2)]],
+  uint vertexID [[vertex_id]],
+  uint instanceID [[instance_id]]) {
+  ObjectVertexOut out;
+  uint layers = max(uniforms.layerCount, 1u);
+  uint objectIndex = instanceID / layers;
+  uint layer = instanceID % layers;
+  MeshVertex vtx = vertices[vertexID];
+  ObjectState state = states[objectIndex];
+
+  float3 position = state.positionAndType.xyz + vtx.position * state.scaleAndPadding.xyz;
+  float4 worldPos = float4(position, 1.0);
+  out.position = uniforms.viewProjectionMatrix * worldPos;
+  out.normal = vtx.normal;
+  out.worldPos = position;
+  out.objectType = state.positionAndType.w;
+  out.layer = layer;
+  return out;
+}
+
+float3 objectColor(float type, float3 pos, float time) {
+  if (type > 0.5) {
+    float hue = 0.52 + 0.05 * sin(time + pos.x);
+    return mix(float3(0.1, 0.35, 0.45), float3(0.18, 0.5, 0.62), 0.5 + 0.5 * sin(hue));
+  }
+  float variation = 0.2 * sin(pos.x * 2.0 + time) + 0.1 * cos(pos.y * 3.0 - time * 0.5);
+  return float3(0.05, 0.2, 0.28) + variation;
+}
+
+fragment float4 objectFragmentShader(
+  ObjectVertexOut in [[stage_in]],
+  constant SceneUniforms &uniforms [[buffer(0)]]) {
+  float3 lightDir = normalize(float3(0.4, 0.9, -0.2));
+  float ndotl = max(dot(in.normal, lightDir), 0.0);
+  float rim = pow(1.0 - max(dot(in.normal, float3(0, 0, 1)), 0.0), 2.0);
+  float3 baseColor = objectColor(in.objectType, in.worldPos, uniforms.time);
+  float3 color = baseColor * (0.35 + ndotl * 0.65) + rim * 0.1;
+  return float4(color, 0.95);
+}
+
+kernel void simulateObjects(
+  device ObjectState *states [[buffer(0)]],
+  constant SimulationUniforms &uniforms [[buffer(1)]],
+  uint id [[thread_position_in_grid]]) {
+  if (id >= uniforms.objectCount) {
+    return;
+  }
+
+  ObjectState state = states[id];
+  float dt = uniforms.deltaTime;
+  float type = state.positionAndType.w;
+
+  float3 velocity = state.velocityAndPhase.xyz;
+  float phase = state.velocityAndPhase.w + dt * (0.5 + 0.5 * type);
+
+  float3 offset = float3(sin(phase * 1.3), cos(phase * 0.8), sin(phase * 1.1)) * 0.02;
+  float buoyancy = mix(0.2, 0.35, type);
+  velocity.y += buoyancy * dt;
+  velocity.x += sin(uniforms.globalTime * 0.2 + state.positionAndType.z) * 0.01 * dt;
+
+  float3 position = state.positionAndType.xyz + velocity * dt + offset;
+
+  if (position.y > 1.2) {
+    position.y = -0.9;
+  }
+  if (position.x > 2.0 || position.x < -2.0) {
+    velocity.x *= -0.8;
+  }
+  if (position.z > -0.3 || position.z < -3.5) {
+    velocity.z *= -0.6;
+  }
+
+  state.positionAndType = float4(position, type);
+  state.velocityAndPhase = float4(velocity, phase);
+  states[id] = state;
 }
